5b912a446a3f9b6d8472dad50f38b9043790f644
Matthew Woods <mxttwoods@gmail.com>
2021-07-20 23:33:44 -0400

    fix order of operations and add logging

--- api/wsgi.py
+++ api/wsgi.py
@@ -9,16 +9,16 @@ app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read("conf.ini")
 
+USERNAME = config.get("settings", "user")
+PASSWORD = config.get("settings", "pass")
+PORT = config.getint("settings", "port")
+SMTP = config.get("settings", "smtp_server")
+
 with open("log.yaml", "r") as conf:
     logger_config = yaml.safe_load(conf.read())
     logging.config.dictConfig(logger_config)
 
 logger = logging.getLogger("main")
-
-USERNAME = config.get("settings", "user")
-PASSWORD = config.get("settings", "pass")
-PORT = config.getint("settings", "port")
-SMTP = config.get("settings", "smtp_server")
 
 logger.info("PORT: %s", PORT)
 logger.info("USERNAME: %s", USERNAME)
@@ -33,6 +33,7 @@ def send_mail(email, subject, message):
         global PASSWORD
         global PORT
         global SMTP
+
         content = """\n
             Hello, World!
 
@@ -52,16 +53,11 @@ def send_mail(email, subject, message):
             server.login(USERNAME, PASSWORD)
             server.sendmail(USERNAME, email, content)
 
-        response = (
-            "Email sent to {email}, subject: {subject}, message: {message}".format(
-                message=message, subject=subject, email=email
-            )
-        )
+        response = "Email sent to {0}, content: {1}".format(email, content)
         logger.info(jsonify(response))
         return response, 200
 
     if request.method == "GET":
-        response = "POST received {0}, subject {1}, message {2}".format(
-            email, subject, message
-        )
-        return f"Mail: {response}", 200
+        response = "Email sent to {0}, content: {1}".format(email, content)
+        logger.info(jsonify(response))
+        return response, 200
